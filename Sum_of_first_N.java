a506631deb6344020916ef59d80350c1a737a121
SoumyadeepPaul23 <76440743+SoumyadeepPaul23@users.noreply.github.com>
2023-05-12 23:19:44 -0700

    Update Sum_of_first_N.java

--- Sum_of_first_N.java
+++ Sum_of_first_N.java
@@ -2,11 +2,11 @@ import java.util.*;
 public class Sum_of_first_N{
     public static int sum(int n)
     {
-        if(n==1)
+        if(n==1)  // base case
         {
             return n;
         }
-        return n+sum(n-1);
+        return n+sum(n-1); // recurssion called
     }
     public static void main(String[] args) {
         int ans=sum(5);
